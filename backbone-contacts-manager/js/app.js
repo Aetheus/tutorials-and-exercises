e0f7e1d2d46f487a5634a2ad735d0817e46f6f1e
Adrian <adrianhyk@gmail.com>
2015-08-25 01:32:02 +0800

    Added basic model, colleciton and view

--- backbone-contacts-manager/js/app.js
+++ backbone-contacts-manager/js/app.js
@@ -1,6 +1,12 @@
 //IIFE that aliases jQuery as $ symbol
 (function($){
 
+    //underscorejs template test
+    var egdata = {name: "Jack"};
+    var egtemplate = "Hi there, <%= name %>!"
+    var output = _.template(egtemplate);
+    console.log(output(egdata));
+
     var Contacts = [
         { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
         { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
@@ -11,6 +17,31 @@
         { name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
         { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
     ];
+
+
+    var Contact = Backbone.Model.extend({
+        defaults: {
+            photo: "/img/placeholder.png"
+        }
+    });
+
+    var Directory = Backbone.Collection.extend({
+        model: Contact
+    });
+
+    var ContactView = Backbone.View.extend({
+        tagName: "article",
+        className: "contact-container",
+        template: $("#contactTemplate").html(),
+
+        render: function (){
+            var tmpl = _.template(this.template);
+
+            this.$el.html(tmpl(this.model.toJSON()));
+
+            return this;
+        }
+    });
 
 
 })(jQuery);
