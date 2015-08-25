a6bf01a37416890811b62da3c3a8ead816170066
Aetheus <adrianhyk@gmail.com>
2015-08-25 13:09:39 +0800

    Removed discarded tutorials. Added a new Backbone tutorial

---
+++ backbone-hello/js/app.js
@@ -0,0 +1,14 @@
+(function($){
+
+    var ListView = Backbone.View.extend({
+        el: $("body"),  //attaches "this.el" to an existing element
+
+        initialize: function (){
+                
+        },
+
+
+    });
+
+
+})(jQuery);
